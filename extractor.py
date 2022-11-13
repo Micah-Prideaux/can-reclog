0daed6d4120f87704ccfdcab95e3a427caee2287
samr-eroad <93617989+samr-eroad@users.noreply.github.com>
2022-11-14 08:39:52 +1000

    Date Search Fix  (#1)
    Fixed the date search

--- extractor.py
+++ extractor.py
@@ -5,10 +5,13 @@ from datetime import datetime
 
 
 def extract(argv):
-    if argv[0] == "pcan":
-        pcan(argv[1:])
-    else:
-        print_useage()
+    try:
+        if argv[0] == "pcan":
+            pcan(argv[1:])
+        else:
+            print_useage()
+    except Exception as e:
+        print(e)
     # do thing
 
 
@@ -22,14 +25,17 @@ def pcan(argv):
                     data = line.split()
                     timedif = float(data[1]) / 1000
                     output.write(f"({(starttime + timedif):.6f}) can0 {data[3]}#{''.join(data[5:])}\n")
-                    """hi"""
                 elif ";---+--   ----+----  --+--  ----+---  +  -+ -- -- -- -- -- -- --" in line:
                     recordingstarted = True
                 elif "Start time:" in line:
                     date_match = re.search(r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2}', line)
-                    timetouple = datetime.strptime(date_match.group(), '%d/%m/%Y %H:%M:%S').timetuple()
-                    starttime = time.mktime(timetouple)
-
+                    if date_match == None:
+                        date_match = re.search(r'\d{1}/\d{2}/\d{4} \d{2}:\d{2}:\d{2}', line)
+                    try:
+                        timetuple = datetime.strptime(date_match.group(), '%d/%m/%Y %H:%M:%S').timetuple()
+                        starttime = time.mktime(timetuple)
+                    except Exception as e:
+                        print(e)
 
 def print_useage():
     print("extractor.py <file type> <file location> <output name>")
